c3ffb57ac2474db70b925e151e9de725c1359bac
Codadillo <leoconr@nuevaschool.org>
2017-12-13 14:30:05 -0800

    Changed PlayerMovement to PlayerControls.

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -2,18 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerControls : MonoBehaviour {
 	public Rigidbody2D rb;
 	public float Speed = 10.0f;
 	// Use this for initialization
 	void Start () {
-		rb = GetComponent<Rigidbody2D>();
+		rb = GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		float Horizontal = Input.GetAxis("Horizontal");
-		float Vertical =  Input.GetAxis("Vertical");
+		Movement ();
+	}
+
+	void Movement () {
+		float Horizontal = Input.GetAxis ("Horizontal");
+		float Vertical = Input.GetAxis ("Vertical");
 		rb.velocity = new Vector2 (Horizontal * Speed, Vertical * Speed);
 	}
 }
