62c758aab6bec7f5e5897475799feef869cd73f3
Codadillo <leoconr@nuevaschool.org>
2017-12-13 14:14:28 -0800

    Added BASIC character movement script

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	public Rigidbody2D rb;
+	public float Speed = 10.0f;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		rb.velocity = new Vector2 (Input.GetAxis("Horizontal") * Speed, Input.GetAxis("Vertical") * Speed);
+	}
+}
