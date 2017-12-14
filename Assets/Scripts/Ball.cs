ac4c5b85edfba5ca67539851e27cecc1c46e0680
Codadillo <leoconr@nuevaschool.org>
2017-12-13 21:15:40 -0800

    Added a simple thing that throws a snowball to show Daniel Unity.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Ball : MonoBehaviour {
+	public Rigidbody2D rb;
+	public float Speed = 1.0f;
+	private String OwnerName;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate () {
+		float Dir = transform.localEulerAngles.z;
+		float XDir = Mathf.Cos (Dir * Mathf.Deg2Rad);
+		float YDir = Mathf.Sin (Dir * Mathf.Deg2Rad);
+		rb.velocity = new Vector2 (XDir, YDir) * Speed;
+	}
+		
+
+}
--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -5,19 +5,46 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour {
 	public Rigidbody2D rb;
 	public float Speed = 10.0f;
+	private Vector3 PlayerRotation;
+	public GameObject Prefab;
+	public static GameObject BallPrefab;
+
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
+		BallPrefab = Prefab;
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		Movement ();
-	}
-
-	void Movement () {
 		float Horizontal = Input.GetAxis ("Horizontal");
 		float Vertical = Input.GetAxis ("Vertical");
-		rb.velocity = new Vector2 (Horizontal * Speed, Vertical * Speed);
+
+		Movement (Horizontal, Vertical);
+		if (Input.GetKeyDown("space")) ThrowBall (this.transform.position, this.transform.rotation); 
 	}
+
+	void Movement (float Horizontal, float Vertical) {
+		//move the player
+		rb.velocity = new Vector2 (Horizontal * Speed, Vertical * Speed);
+
+		//change rotation of player
+		if (Horizontal > 0) {
+			PlayerRotation.z = 0.0f;
+		} else if (Horizontal < 0) {
+			PlayerRotation.z = 180.0f;
+		}
+		if (Vertical > 0) {
+			PlayerRotation.z = 90.0f;
+		} else if (Vertical < 0) {
+			PlayerRotation.z = -90.0f;
+		}
+		this.transform.eulerAngles = PlayerRotation;
+	}
+
+	public static void ThrowBall (Vector3 Position, Quaternion Rotation) {
+		Instantiate (BallPrefab, Position, Rotation);
+	}
+
+
 }
